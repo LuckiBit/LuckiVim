70360469c86f8eac40b31784fdc1dd9d9c4ec376
LuckiBit <luckijiabao@gmail.com>
2026-02-25 01:27:33 +0800

    feat: add tokyonight theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -5,11 +5,12 @@ return {
         priority = 1000,
 
         opts = {
-            style = "moon", -- storm | night | day | moon
+            style = "moon" -- storm | night | day | moon
         },
 
-        config = function()
+        config = function(_, opts)
+            require("tokyonight").setup(opts)
             vim.cmd.colorscheme("tokyonight")
-        end,
-    },
+        end
+    }
 }
