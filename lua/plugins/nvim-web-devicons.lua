d688366504b86ba1ddd9b8cfc4569dd9052ea69d
LuckiBit <luckijiabao@gmail.com>
2026-03-03 00:03:55 +0800

    feat: update nvim-web-devicons.lua

--- lua/plugins/nvim-web-devicons.lua
+++ lua/plugins/nvim-web-devicons.lua
@@ -1,10 +1,45 @@
 return {
     "nvim-tree/nvim-web-devicons",
     lazy = true,
+
     opts = {
+        override = {
+            zsh = {
+                icon = "",
+                color = "#428850",
+                cterm_color = "65",
+                name = "Zsh",
+            },
+        },
+
         default = true, -- show default icon if no match
         color_icons = true, -- enable colored icons
         strict = true, -- strict filename/extension matching
         blend = 0, -- prevent width issues in popup menus
+
+        override_by_filename = {
+            [".gitignore"] = {
+                icon = "",
+                color = "#f1502f",
+                name = "Gitignore",
+            },
+        },
+
+        override_by_extension = {
+            ["log"] = {
+                icon = "",
+                color = "#81e043",
+                name = "Log",
+            },
+        },
+
+        override_by_operating_system = {
+            ["apple"] = {
+                icon = "",
+                color = "#A2AAAD",
+                cterm_color = "248",
+                name = "Apple",
+            },
+        },
     },
 }
